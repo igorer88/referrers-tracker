5d908f9d14cb8d01fe879ab8a855460e7104e3df
Igor Escalona <igorer88@gmail.com>
2021-01-17 05:49:37 -0400

    Add and modify config files

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,14 +1,23 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
+  plugins: ['html', 'vue'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'warn',
+    //disable rule of space before function parentheses
+    'space-before-function-paren': ['warn', 'never'],
+    // allow paren-less arrow functions
+    'arrow-parens': ['error', 'as-needed'],
+    // allow async-await
+    'generator-star-spacing': 'warn'
   }
 };
---
+++ client/vue.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+process.env.VUE_APP_VERSION = require('../client/package.json').version;
+
+module.exports = {
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    noInfo: true,
+    overlay: true,
+    open: 'firefox-dev'
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = process.env.VUE_APP_TITLE;
+      return args;
+    });
+  }
+};
