65606a01770deea39d31fa5bf9154b01d881da5a
Igor Escalona <igorer88@gmail.com>
2021-01-17 19:48:45 -0400

    refactor and fixes

--- routes/middleware.js
+++ routes/middleware.js
@@ -4,35 +4,44 @@ const jwt = require('jwt-simple');
 const moment = require('moment');
 
 const checkToken = (req, res, next) => {
-    if (!req.header('access-token')){
-        return res.status(406).json({
-            code: 406,
-            status: 'Error',
-            message: 'You must include the access-token header'
-        });
-    }
-    const token = req.header('access-token');
-    let payload = null;
-    try {
-        payload = jwt.decode(token, process.env.TOKEN_KEY);
-    } catch (error) {
-        return res.status(401).json({
-            code: 401,
-            status: 'Error',
-            message: 'Invalid token'
-        });
-    }
-    if(moment().unix() > payload.expire_at){
-        return res.status(401).json({
-            code: 401,
-            status: 'Error',
-            message: 'Expired token'
-        });
-    }
-    req.userId = payload.userId;
-    next();
-}
+  if (!req.header('Authorization')) {
+    return res.status(406).json({
+      code: 406,
+      status: 'Error',
+      message: 'You must include the Authorization header',
+    });
+  }
+  const header = req.header('Authorization');
+  const token = header.split(' ');
+  if (token[0] !== 'Bearer') {
+    return res.status(401).json({
+      code: 401,
+      status: 'Error',
+      message: 'Invalid token format',
+    });
+  }
+  let payload = null;
+
+  try {
+    payload = jwt.decode(token[1], process.env.TOKEN_KEY);
+  } catch (error) {
+    return res.status(401).json({
+      code: 401,
+      status: 'Error',
+      message: 'Invalid token',
+    });
+  }
+  if (moment().unix() > payload.expireAt) {
+    return res.status(401).json({
+      code: 401,
+      status: 'Error',
+      message: 'Expired token',
+    });
+  }
+  req.userId = payload.userId;
+  next();
+};
 
 module.exports = {
-    checkToken
+  checkToken,
 };
--- routes/url.controller.js
+++ routes/url.controller.js
@@ -7,46 +7,81 @@ const createUrl = async (req, res) => {
   try {
     // eslint-disable-next-line no-extra-boolean-cast
     // if (services.validateUrl(req.body.url))
-      //  return res.status(400).send({ code: 400, message: 'Invalid URL.' });
-      const link = new Url(req.body.url);      
-      await urlSchema.create({ pathname: link.href });
-      return res.status(201).send({ code: 201, status: 'Success', message: link });
+    //  return res.status(400).send({ code: 400, message: 'Invalid URL.' });
+    const link = new Url(req.body.url);
+    await urlSchema.create({ pathname: link.href });
+    return res
+      .status(201)
+      .send({ code: 201, status: 'Success', message: link });
   } catch (error) {
-      if (error.Code === 11000) {
-        return res.status(409).send({ code: 409, status: 'Error', message: 'The url already exist.' });
-      }
-      console.error(error);
-      return res.status(500).send({ code: 500, status: 'Error', message: 'Something went wrong. Please try again.' });
+    if (error.code === 11000) {
+      return res.status(409).send({
+        code: 409,
+        status: 'Error',
+        message: 'The url already exist.',
+      });
+    }
+    console.error(error);
+    return res.status(500).send({
+      code: 500,
+      status: 'Error',
+      message: 'Something went wrong. Please try again.',
+    });
   }
 };
 
 const updateClicks = async (req, res) => {
   try {
-      const url = await urlSchema.findOne({ pathname: req.body.url });
-      if (!url) {
-        return res.status(404).send({ code: 404, status: 'Error', message: 'Not found'});
-      }
-      url.clicks += 1;
-      let updated = await urlSchema.findOneAndUpdate({pathname: url.pathname}, {clicks: url.clicks});
-      return res.status(200).send({ code: 200, status: 'Success', message: updated });
-
+    const url = await urlSchema.findOne({ pathname: req.body.url });
+    if (!url) {
+      return res
+        .status(404)
+        .send({ code: 404, status: 'Error', message: 'Url not found' });
+    }
+    url.clicks += 1;
+    let updated = await urlSchema.findOneAndUpdate(
+      { pathname: url.pathname },
+      { clicks: url.clicks }
+    );
+    updated.__v = undefined;
+    return res
+      .status(200)
+      .send({ code: 200, status: 'Success', message: updated });
   } catch (error) {
-      return res.status(500).send({ code: 500, status: 'Error', message: 'Something went wrong. Please try again.' });
+    return res.status(500).send({
+      code: 500,
+      status: 'Error',
+      message: 'Something went wrong. Please try again.',
+    });
   }
 };
 
 const topHighiestClicks = async (req, res) => {
   try {
     const tops = await urlSchema.find({}).sort({ clicks: -1 }).limit(3);
-    
-    return !tops ? res.status(200).send({ code: 200, message: 0 }) : res.status(200).send({ code: 200, message: tops });
+    console.log(tops);
+    return tops.length === 0
+      ? res.status(404).send({
+          code: 404,
+          message: 'No urls have been found',
+          payload: tops,
+        })
+      : res.status(200).send({
+          code: 200,
+          message: `${tops.length} urls have been found`,
+          payload: tops,
+        });
   } catch (error) {
-      return res.status(500).send({ code: 500, status: 'Error', message: 'Something went wrong. Please try again.' });
+    return res.status(500).send({
+      code: 500,
+      status: 'Error',
+      message: 'Something went wrong. Please try again.',
+    });
   }
 };
 
 module.exports = {
   createUrl,
   updateClicks,
-  topHighiestClicks
+  topHighiestClicks,
 };
