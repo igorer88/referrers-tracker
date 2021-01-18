f1ab4e90d82cc87685b9c6f7b5a8aafcdc431f0c
Igor Escalona <igorer88@gmail.com>
2021-01-18 00:48:49 -0400

    Add Login and Signup views

---
+++ client/src/views/Login.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <section>
+      <div class="hero is-primary">
+        <div class="hero-body">
+          <h1 class="title has-text-centered is-size-2">
+            Login to Referrers Tracker
+          </h1>
+          <div class="columns is-centered">
+            <div class="column is-half">
+              <div class="notification is-light">
+                <form @submit.stop.prevent="onSubmit">
+                  <b-field label="Username">
+                    <b-input
+                      icon-pack="fas"
+                      icon="user"
+                      v-model="form.username"
+                      :placeholder="'username'"
+                    ></b-input>
+                  </b-field>
+                  <b-field label="Password">
+                    <b-input
+                      type="password"
+                      icon-pack="fas"
+                      icon="lock"
+                      v-model="form.password"
+                      password-reveal
+                      :placeholder="'password'"
+                      aria-describedby="input-password-live-feedback"
+                    >
+                    </b-input>
+                  </b-field>
+                  <div class="columns has-text-centered">
+                    <div class="column">
+                      <b-button
+                        class="is-info is-rounded is-outlined is-medium"
+                        tag="input"
+                        native-type="submit"
+                        value="Login"
+                      />
+                    </div>
+                  </div>
+                </form>
+                <br />
+                <a href="#/signup">
+                  <span>Don't you have a account? <strong>Signup</strong></span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line
+import { /*mapActions,*/ mapState } from 'vuex';
+import axios from '@/config/axios';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      form: {
+        username: null,
+        password: null
+      }
+    };
+  },
+  computed: {
+    ...mapState({
+      // authStatus: state => state.auth.profile.status
+    })
+  },
+  methods: {
+    resetForm() {
+      this.form = {
+        username: null,
+        password: null
+      };
+    },
+    onSubmit() {
+      axios
+        .post(`auth/login`, this.form)
+        .then(response => {
+          // commit("onLoginSuccess", response);
+          console.log(response.data);
+          this.$buefy.toast.open({
+            duration: 5000,
+            message: `${response.data.message}`,
+            position: 'is-bottom',
+            type: 'is-success'
+          });
+          const payload = response.data.payload;
+          console.log(payload);
+          localStorage.setItem('access-token', payload.token);
+          this.$emit('authenticated', true);
+          this.$router.replace({
+            name: 'Home',
+            params: { user: payload.user }
+          });
+        })
+        .catch(error => {
+          // commit("onLoginError", error);
+          const err = error.response.data;
+          this.$buefy.toast.open({
+            duration: 5000,
+            message: `${err.message}`,
+            position: 'is-bottom',
+            type: 'is-danger'
+          });
+          this.resetForm();
+        });
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+body {
+  padding: 0;
+  margin: 0;
+}
+a {
+  text-decoration: none;
+}
+.hero {
+  height: 100vh;
+  position: relative;
+}
+.notification {
+  padding-top: 20px;
+  padding-bottom: 30px;
+}
+.button {
+  margin-top: 10px;
+}
+</style>
---
+++ client/src/views/Signup.vue
@@ -0,0 +1,168 @@
+<template>
+  <div>
+    <section>
+      <div class="hero is-primary">
+        <div class="hero-body">
+          <h1 class="title has-text-centered is-size-2">
+            Signup to Referrers Tracker
+          </h1>
+          <div class="columns is-centered">
+            <div class="column is-half">
+              <div class="notification is-light">
+                <form @submit.stop.prevent="onSubmit">
+                  <b-field label="Username">
+                    <b-input
+                      icon-pack="fas"
+                      icon="user"
+                      v-model="form.username"
+                      :placeholder="'username'"
+                      maxlength="20"
+                    ></b-input>
+                  </b-field>
+                  <b-field label="Password">
+                    <b-input
+                      type="password"
+                      icon-pack="fas"
+                      icon="lock"
+                      password-reveal
+                      v-model="form.password"
+                      :placeholder="'password'"
+                      aria-describedby="input-password-live-feedback"
+                    >
+                    </b-input>
+                  </b-field>
+                  <b-field
+                    label="Confirm password"
+                    :type="{ 'is-success': checkPassword }"
+                    :message="checkPassword"
+                  >
+                    <b-input
+                      type="password"
+                      icon-pack="fas"
+                      icon="lock"
+                      password-reveal
+                      v-model="form.passwordConfirm"
+                      :placeholder="'password'"
+                      aria-describedby="input-password-live-feedback"
+                    >
+                    </b-input>
+                  </b-field>
+                  <div class="columns has-text-centered">
+                    <div class="column">
+                      <b-button
+                        class="is-warning is-light is-rounded is-outlined is-medium"
+                        native-type="reset"
+                        @click="resetForm()"
+                      >
+                        Reset
+                      </b-button>
+                    </div>
+                    <div class="column">
+                      <b-button
+                        class="is-info is-rounded is-outlined is-medium"
+                        tag="input"
+                        native-type="submit"
+                        value="Signup"
+                        :loading="loading"
+                      />
+                    </div>
+                  </div>
+                </form>
+                <br />
+                <a href="#/login"><span>Already registered? Login</span></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line
+import { /*mapActions,*/ mapState } from 'vuex';
+import axios from '@/config/axios';
+
+export default {
+  name: 'Signup',
+  data() {
+    return {
+      form: {
+        username: null,
+        password: null,
+        passwordConfirm: null
+      },
+      confirmed: false,
+      loading: false
+    };
+  },
+  computed: {
+    ...mapState({
+      // authStatus: state => state.auth.profile.status
+    }),
+    checkPassword: function() {
+      return this.form.password === this.form.passwordConfirm
+        ? 'Password matches!'
+        : 'Password must match!';
+    }
+  },
+  methods: {
+    resetForm() {
+      this.form = {
+        username: null,
+        password: null,
+        passwordConfirm: null
+      };
+      this.confirmed = false;
+    },
+    onSubmit() {
+      this.loading = true;
+      axios
+        .post(`auth/signup`, this.form)
+        .then(response => {
+          // commit("onLoginSuccess", response);
+          console.log(response.data);
+          this.$buefy.toast.open({
+            duration: 5000,
+            message: `${response.data.message}`,
+            position: 'is-bottom',
+            type: 'is-success'
+          });
+          this.$router.replace({ name: 'Login' });
+        })
+        .catch(error => {
+          // commit("onLoginError", error);
+          const err = error.response.data;
+          this.$buefy.toast.open({
+            duration: 5000,
+            message: `${err.message}`,
+            position: 'is-bottom',
+            type: 'is-danger'
+          });
+        });
+      this.loading = false;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+body {
+  padding: 0;
+  margin: 0;
+}
+a {
+  text-decoration: none;
+}
+.hero {
+  height: 100vh;
+  position: relative;
+}
+.notification {
+  padding-top: 20px;
+  padding-bottom: 30px;
+}
+.button {
+  margin-top: 10px;
+}
+</style>
