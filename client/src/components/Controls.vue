5a517d58255245de919ca02f373a424cea131da6
Igor Escalona <igorer88@gmail.com>
2021-01-18 00:39:27 -0400

    Add urls list components

---
+++ client/src/components/Controls.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block m-5">
+    <b-button type="is-primary" @click="$parent.loadUrls()">
+      <b-icon
+        icon="sync-alt"
+        :class="{ 'fa-spin': loading }"
+        size="is-medium"
+      ></b-icon>
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Controls',
+  props: ['loading']
+};
+</script>
+
+<style></style>
---
+++ client/src/components/TopUrls.vue
@@ -0,0 +1,80 @@
+<template>
+  <section>
+    <div class="card has-text-centered">
+      <Controls :loading="loading" />
+      <div
+        class="block mb-5"
+        v-show="!loading"
+        v-for="el in topUrlList"
+        :key="el._id"
+      >
+        <a
+          target="_blank"
+          rel="noopener"
+          :href="el.pathname"
+          @click="clickHandler(el.pathname)"
+        >
+          {{ el.pathname + ' - ' + el.clicks + ' Clicks' }}
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import axios from '@/config/axios';
+import Controls from '@/components/Controls';
+
+export default {
+  name: 'TopUrls',
+  components: { Controls },
+  data() {
+    return {
+      loading: false,
+      topUrlList: []
+    };
+  },
+  mounted() {
+    this.loadUrls();
+  },
+  methods: {
+    loadUrls() {
+      this.loading = true;
+      axios
+        .get('urls/tops')
+        .then(response => {
+          console.log(response.data);
+          this.topUrlList = response.data.payload;
+          this.loading = false;
+        })
+        .catch(error => {
+          console.log(error);
+          const err = error.response.data;
+          this.$buefy.toast.open({
+            duration: 5000,
+            message: `${err.message}`,
+            position: 'is-bottom',
+            type: 'is-danger'
+          });
+        });
+    },
+    clickHandler(url) {
+      axios
+        .put('urls', { url })
+        .then(response => {
+          console.log(response.data);
+          this.loadUrls();
+        })
+        .catch(error => {
+          const err = error.response.data;
+          this.$buefy.toast.open({
+            duration: 5000,
+            message: `${err.message}`,
+            position: 'is-bottom',
+            type: 'is-danger'
+          });
+        });
+    }
+  }
+};
+</script>
+<style></style>
