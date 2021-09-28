0848eb4825bbe44148a68c1a6ea38df8e91cf293
Igor Escalona <igorer88@gmail.com>
2021-09-28 09:08:03 -0400

    Fix pacakge.json import

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-process.env.VUE_APP_VERSION = require('../client/package.json').version;
+process.env.VUE_APP_VERSION = require('./package.json').version;
 
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
